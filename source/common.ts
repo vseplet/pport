d27b9948805d578cbe2167be1c6e5c547b09da7f
Vsevolod Pletnev <vseplet@gmail.com>
2025-04-27 02:06:07 +0700

    add last messages view

--- source/common.ts
+++ source/common.ts
@@ -9,11 +9,11 @@ export const baseURL = Deno.permissions.querySync &&
 export const domain = baseURL.split("//")[1];
 
 export const introText = `
-                   ____  ____   ___  ____ _____
-                  |  _ \\|  _ \\ / _ \\|  _ \\_   _|
-                  | |_) | |_) | | | | |_) || |
-                  |  __/|  __/| |_| |  _ < | |
-                  |_|   |_|    \\___/|_| \\_\\|_|
+       ____  ____   ___  ____ _____
+      |  _ \\|  _ \\ / _ \\|  _ \\_   _|
+      | |_) | |_) | | | | |_) || |
+      |  __/|  __/| |_| |  _ < | |
+      |_|   |_|    \\___/|_| \\_\\|_|
 
       📜 Text-based Messenger for the Command Line
 
@@ -25,71 +25,6 @@ export const introText = `
 
       Specially crafted for Hacker News ❤️
       https://news.ycombinator.com/item?id=43805189
-`;
-
-// deno-fmt-ignore
-export const home = async () => `
-<html>
-  <head>
-    <title>PPORT</title>
-    <style>
-      * {
-        background:rgb(0, 68, 74);
-        color: white;
-        font-family: monospace;
-      }
-
-      pre {
-      font-size: 14px;
-        line-height: 1.4;
-        white-space: pre;
-      }
-    </style>
-  </head>
-  <body>
-    <pre>
-
-      ${
-        introText.replace(
-          /https?:\/\/[^\s]+/g,
-          (url) => `<a href="${url}">${url}</a>`,
-        )
-      }
-                      <span style="color: orange">Total installs: ${(await kv.get(["installs"])).value}</span>
-
-
-
-      Install / Update
-
-        Unix-like:
-
-        $ curl -fsSL ${domain} | sh
-
-        Windows:
-
-        $ irm ${domain} | iex
-
-      Run
-
-        $ pport
-    </pre>
-
-    <script type="text/javascript" >
-      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-      m[i].l=1*new Date();
-      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-      ym(101415234, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-      });
-    </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/101415234" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-  </body>
-</html>
 `;
 
 export const installCommand =
--- source/server.ts
+++ source/server.ts
@@ -1,8 +1,86 @@
-import { home, kv, Message, psScript, shScript } from "./common.ts";
+import {
+  domain,
+  introText,
+  kv,
+  Message,
+  psScript,
+  shScript,
+} from "./common.ts";
 import { stripAnsiAndControlChars } from "./tui.ts";
 
 const messages: Array<Message> = [];
 const channel = new BroadcastChannel("all_messages");
+
+// deno-fmt-ignore
+export const home = async () => `
+<html>
+  <head>
+    <title>PPORT</title>
+    <style>
+      * {
+        background:rgb(0, 68, 74);
+        color: white;
+        font-family: monospace;
+      }
+
+      pre {
+      font-size: 14px;
+        line-height: 1.4;
+        white-space: pre;
+      }
+    </style>
+  </head>
+  <body>
+    <pre>
+
+      ${
+        introText.replace(
+          /https?:\/\/[^\s]+/g,
+          (url) => `<a href="${url}">${url}</a>`,
+        )
+      }
+                      <span style="color: orange">Total installs: ${(await kv.get(["installs"])).value}</span>
+
+      Install / Update
+
+        Unix-like:
+
+        $ curl -fsSL ${domain} | sh
+
+        Windows:
+
+        $ irm ${domain} | iex
+
+      Run
+
+        $ pport
+
+      <code style="color: rgb(0 251 255)">
+      Last messages:
+
+${messages.map(msg => {
+  return `        ${msg.username}: ${msg.text}\n`
+}).join('')}
+      </code>
+    </pre>
+
+    <script type="text/javascript" >
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+      ym(101415234, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+      });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/101415234" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+  </body>
+</html>
+`;
 
 const updateInstallsCount = async () => {
   try {
